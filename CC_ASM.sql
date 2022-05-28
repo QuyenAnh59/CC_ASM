6d6b109f61505da207cf118b68fb0bdc0712752a
QuyenAnh59 <105089743+QuyenAnh59@users.noreply.github.com>
2022-05-28 10:09:03 +0700

    Update CC_ASM.sql

--- CC_ASM.sql
+++ CC_ASM.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th5 28, 2022 lúc 04:28 AM
+-- Thời gian đã tạo: Th5 28, 2022 lúc 04:46 AM
 -- Phiên bản máy phục vụ: 10.4.22-MariaDB
 -- Phiên bản PHP: 7.3.33
 
@@ -42,6 +42,30 @@ INSERT INTO `product` (`Product ID`, `Product Name`, `Product Price`, `Product I
 (0, 'asd', 'asd', ''),
 (5, 'Canon', '15000', 'bg.jfif');
 
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
+(1, 'QUYEN ANH', '123'),
+(2, 'ANH', '12345'),
+(3, 'HAI', '123456'),
+(4, 'QA', '1234'),
+(6, 'Loan', '123'),
+(33, 'demo', 'demo');
+
 --
 -- Chỉ mục cho các bảng đã đổ
 --
@@ -51,6 +75,12 @@ INSERT INTO `product` (`Product ID`, `Product Name`, `Product Price`, `Product I
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`Product ID`);
+
+--
+-- Chỉ mục cho bảng `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
